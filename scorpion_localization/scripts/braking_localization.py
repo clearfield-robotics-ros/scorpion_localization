6fc7b3cb49103d162d1087717ee531dfdb92f626
Aaron <aaaaaaronchong@gmail.com>
2018-04-30 16:02:42 -0400

    working utm to latlon

--- scorpion_localization/scripts/braking_localization.py
+++ scorpion_localization/scripts/braking_localization.py
@@ -10,6 +10,7 @@ from robot_localization.srv import SetPose
 import math
 from geopy import Point
 from geopy.distance import distance, VincentyDistance
+import utm
 
 def update_brakes(data):
     global past_brake_flag
@@ -40,30 +41,31 @@ def update_gps(data):
     gps_to_set = data
 
 
-def get_mine_location():
-    quat            = pose_to_set.pose.pose.orientation
-    quat            = (quat.x, quat.y, quat.z, quat.w)
-    euler           = tf.transformations.euler_from_quaternion(quat)
-    #roll, pitch, yaw
+# def get_mine_location():
+#     quat            = pose_to_set.pose.pose.orientation
+#     quat            = (quat.x, quat.y, quat.z, quat.w)
+#     euler           = tf.transformations.euler_from_quaternion(quat)
+#     #roll, pitch, yaw
 
-    # R               = 6378.1
-    bearing         = euler[2]
-    d               = 0.00115 #km
-    # distMiles       = 0.0007145768711 #miles
-    lat1            = gps_to_set.latitude
-    lon1            = gps_to_set.longitude
-    # latlong         = VincentyDistance(kilometers=d).destination(Point(lat1, lon1), bearing).format_decimal()
-    destination     = VincentyDistance(kilometers=d).destination(Point(lat1, lon1), bearing)
+#     # R               = 6378.1
+#     bearing         = euler[2]
+#     d               = 0.00115 #km
+#     # distMiles       = 0.0007145768711 #miles
+#     lat1            = gps_to_set.latitude
+#     lon1            = gps_to_set.longitude
+#     # latlong         = VincentyDistance(kilometers=d).destination(Point(lat1, lon1), bearing).format_decimal()
+#     destination     = VincentyDistance(kilometers=d).destination(Point(lat1, lon1), bearing)
+#     # lat2, lon2      = utm.to_latlon(340000, 5710000, 17, 'T')
     
-    global mine_gps
-    mine_gps        = gps_to_set
-    mine_gps.latitude   = destination.latitude
-    mine_gps.longitude  = destination.longitude
-    # print latlong[0]
-    # print latlong[1]
-    # print mine_gps.latitude
-    # print mine_gps.longitude
-    return
+#     global mine_gps
+#     mine_gps        = gps_to_set
+#     mine_gps.latitude   = destination.latitude
+#     mine_gps.longitude  = destination.longitude
+#     # print latlong[0]
+#     # print latlong[1]
+#     # print mine_gps.latitude
+#     # print mine_gps.longitude
+#     return
 
 
 def main():
@@ -82,19 +84,28 @@ def main():
     rospy.Subscriber('/gps/filtered', NavSatFix, update_gps)
     rospy.Subscriber('/braking_desired_state', Int16, update_brakes)
     rospy.wait_for_service('/set_pose')
-    set_pose = rospy.ServiceProxy('/set_pose', SetPose)
+    set_pose            = rospy.ServiceProxy('/set_pose', SetPose)
 
+    listener            = tf.TransformListener()
     mine_location_pub   = rospy.Publisher('/gps/mines', NavSatFix, queue_size=10)
 
-    r           = rospy.Rate(30)
+    r                   = rospy.Rate(30)
     while not rospy.is_shutdown():
         if (brake_flag == 1):
             set_pose(pose_to_set)
         if (pub_mine_flag == 1):
-            # mine_location_pub.publish(gps_to_set) #this publishes the scorpion gps
-            get_mine_location()
-            mine_location_pub.publish(mine_gps) #this publises the mine after vincenty distance offset
-            pub_mine_flag = 0
+            try:
+                (trans,rot)         = listener.lookupTransform('utm', 'mine_marker', rospy.Time(0))
+                lat, lon            = utm.to_latlon(trans[0], trans[1], 17, 'T')
+                mine_gps            = gps_to_set
+                mine_gps.latitude   = lat
+                mine_gps.longitude  = lon
+                mine_location_pub.publish(mine_gps)
+                pub_mine_flag       = 0
+
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+            
         r.sleep()
     return
 
